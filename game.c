f5dfea8f2c41e5537639f852e8ac9df813bd8a0d
Samuel Asher Crawley <scr70@cs18250kq.canterbury.ac.nz>
2018-10-09 11:48:38 +1300

    game init

--- game.c
+++ game.c
@@ -1,14 +1,47 @@
 #include "system.h"
+#include "ledmat.h"
+#include "ir_uart.h"
+#include "pacer.h"
+#include "navswitch.h"
+#include "tinygl.h"
+#include "../fonts/font5x7_1.h"
+
+#define PACER_RATE 500
+#define MESSAGE_RATE 10
+
+void display_character (char character)
+{
+    char buffer[2];
+    buffer[0] = character;
+    buffer[1] = '\0';
+    tinygl_text (buffer);
+}
+
 
 int main (void)
 {
     system_init ();
+    led_init ();
+    navswitch_init ();
+    pacer_init (PACER_RATE);
+
+    tinygl_init (PACER_RATE);
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+    tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
 
 
     while (1)
     {
+        pacer_wait();
+        tinygl_update();
+        navswitch_update();
+
+
 
 
 
     }
+
+    return 0;
 }
