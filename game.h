0bae04876ce4b676bf46396b7e582cda3e132dce
ltr28 <ltr28@uclive.ac.nz>
2018-10-13 17:46:44 +1300

    Added comments

--- game.h
+++ game.h
@@ -1,4 +1,10 @@
-
+/*
+    * File:     game.h
+    * Authors:  Samuel Crawley (43420688)
+    *           Luke Trenberth (47277086)
+    * Date:     13.10.18
+    * Brief:    Runs Paper Scissors Rock
+*/
 
 #ifndef GAME_H
 #define GAME_H
@@ -17,5 +23,12 @@
 #define MESSAGE_RATE 30
 
 
-/*-----------------------Main function--------------------------------*/
+/*
+Runs a game of paper scissors rock.
+An initial selection of the number of rounds comes up first. The message continues until the middle button is pressed. The navigation switch up and down buttons can toggle between the options, choosing best of 1, 3 or 5.
+
+For a round, a selection is make by toggling up and down, finalizing a selection by pushing down on the navigation button.
+
+The winner of each round and the overall round is displayed on the LED matrix.
+*/
 int main (void)
--- ingame.c
+++ ingame.c
@@ -1,16 +1,24 @@
-/*---------------------In Game functions------------------------------*/
-
+/*
+    * File:     ingame.c
+    * Authors:  Samuel Crawley (43420688)
+    *           Luke Trenberth (47277086)
+    * Date:     13.10.18
+    * Brief:    Files required to run 1 round
+*/
 #include "ingame.h"
 #include "auxiliary.h"
 
+/*---------------------In Game functions------------------------------*/
 int decide_winner (char input1, char input2)
 {
-    //Who wins the game?
-    //input: 'P'-Paper, 'S'-Scissors, 'R'-Rock
-    //output: 1-Player1 won, 2-Player2 won
-    //output: 0-Error/Draw
+    /*Who wins the game?
+    input: 'P'-Paper, 'S'-Scissors, 'R'-Rock
+    1-Player1 won, 2-Player2 won
+    0-Draw
+    */
     int winner = 0;
     if (input1 == 'P') {
+        //Player 1 selects paper
         if (input2 == 'S') {
             winner = 2;
         } else if (input2 == 'R') {
@@ -19,6 +27,7 @@ int decide_winner (char input1, char input2)
             winner = 0;
         }
     } else if (input1 == 'S') {
+        //Player 1 selects Scissors
         if (input2 == 'R') {
             winner = 2;
         } else if (input2 == 'P') {
@@ -27,6 +36,7 @@ int decide_winner (char input1, char input2)
             winner = 0;
         }
     } else if (input1 == 'R') {
+        //Player 1 selects Rock
         if (input2 == 'P') {
             winner = 2;
         } else if (input2 == 'S') {
@@ -36,35 +46,42 @@ int decide_winner (char input1, char input2)
         }
     } else {
         winner = 0;
+        // Error catching
     }
     return winner;
 }
 
 int exchange_options(char item_options[])
 {
+    /*
+    Takes player1 and player2's options and finds winner, taking in options as inputs
+    */
     int i = 0;
     int winner = 0;
     char their_item = 'a';
     char item_chosen = 0;
     while (item_chosen == 0 || their_item == 'a') {
-        i= increment_options(item_options, i);
+        //While loop continues until both players have selected an input
+        i= increment_options(item_options, i); // Chooses the display option
         if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+            //If nav button is pushed, select item and send output to other controller
             item_chosen = item_options[i];
             ir_uart_putc(item_chosen);
         }
         if (ir_uart_read_ready_p()) {
+            //If a signal is received, set player 2's selection to the received char
             their_item = ir_uart_getc();
         }
     }
-    winner = decide_winner(item_chosen, their_item);
+    winner = decide_winner(item_chosen, their_item); //Finds winner from 2 selections
     return winner;
 }
 
 
 char play_round(void)
 {
-    char char_options[] = {'P', 'S', 'R'};
-    int winner = exchange_options(char_options);
+    char char_options[] = {'P', 'S', 'R'}; //Chooses P, S, R options
+    int winner = exchange_options(char_options); //Finds the winner
 
     return winner;
 }
